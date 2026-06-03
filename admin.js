e88004f0d16ab2fc4a8238cb5f7e0ec45cb84f01
Ricardo Coelho de Villela <ricardovilela.re@gmail.com>
2026-06-03 22:59:19 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://z-imoveis.onrender.com/api';
 let authToken = localStorage.getItem('authToken');
 
 // ==================== AUTH FUNCTIONS ====================
